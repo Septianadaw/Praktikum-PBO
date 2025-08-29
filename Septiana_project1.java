dc0d58bde72bf4d4ee9c7784ac74a2ffbe4f01a3
Septianadaw <septianadaw460@gmail.com>
2025-08-29 22:30:43 +0700

    Add files via upload

---
+++ Septiana_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.septiana_project1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Septiana_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama   : Septiana Diyah Ayu Wulandari");
+        System.out.println("NPM    : 2410506013");
+        System.out.println("Alamat : Klaten, Jawa Tengah");
+        System.out.println("No_HP  : 081234567890");
+    }
+}
