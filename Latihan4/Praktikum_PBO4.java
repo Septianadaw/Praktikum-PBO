bdf2aa6d63795bd4e3d5182def7c7ebcd17cef84
Septianadaw <septianadaw460@gmail.com>
2025-09-12 23:41:49 +0700

    Add files via upload

---
+++ Latihan4/Praktikum_PBO4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package praktikum4;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Praktikum_PBO4 {
+
+    public static void main(String[] args) {
+        // Membuat objek mobil
+        Mobil mobil1 = new Mobil("Honda Civic", 220, "Bensin", 4);
+        
+        // Menampilkan informasi mobil
+        mobil1.tampilkanInfoMobil();
+    }
+}
