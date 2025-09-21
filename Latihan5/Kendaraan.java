e589bd9b04afd8aabd6ed06fb52ae40c2ed17ea4
Septianadaw <septianadaw460@gmail.com>
2025-09-21 13:46:13 +0700

    Add files via upload

---
+++ Latihan5/Kendaraan.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum5;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Kendaraan {
+    String nama;
+    int kecepatan;
+    
+    public void tampilkanInfo() {
+        System.out.println();
+        System.out.println("Nama Kendaraan: " + nama);
+        System.out.println("Kecepatan: " + kecepatan + " km/jam");
+    }
+}
+
+// Kelas Turunan Mobil
+class Mobil extends Kendaraan {
+    int jumlahPintu;
+    
+    @Override
+    public void tampilkanInfo() {
+        super.tampilkanInfo();
+        System.out.println("Jumlah Pintu: " + jumlahPintu);
+    }
+}
+
+// Kelas Turunan SepedaMotor 
+class Motor extends Kendaraan {
+    String jenisMesin;
+    
+    @Override
+    public void tampilkanInfo() {
+        super.tampilkanInfo();
+        System.out.println("Jenis Mesin: " + jenisMesin);
+    }
+}
