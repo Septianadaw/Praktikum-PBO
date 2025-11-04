7103db761a148c52a908788af29b7415b6a3b20a
Septianadaw <septianadaw460@gmail.com>
2025-11-04 08:49:36 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,33 +1,29 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package praktikum3;
+package praktikum9;
 
 /**
  *
- * @author ASUS
+ * @author acer
  */
 public class Main {
     public static void main(String[] args) {
-        Mobil mobil1 = new Mobil("Toyota", "Avanza", 2019);
-        Mobil mobil2 = new Mobil("Honda", "Civic", 2020);
-        mobil1.info();
-        mobil1.startEngine();
+        Kendaraan mobil = new Mobil();
+        Kendaraan sepeda = new Sepeda();
         
-        System.out.println();
+        mobil.berjalan();
+        mobil.info();
         
-        mobil2.info();
-        mobil2.startEngine();
-         
-        System.out.println();
+        sepeda.berjalan();
+        sepeda.info();
         
-        mobil1.setWarna("Merah");
-        mobil2.setWarna("Hitam");
+        System.out.println("\nPercobaan membuat objek langsung dari kelas abstrak: ");
         
-        mobil1.info();
-        System.out.println();
-        mobil2.info();
+        System.out.println("Percobaan: Kendaraan kendaraan = new Kendaraan();");
+        System.out.println("Hasil: ERROR karena Kelas Kendaraan adalah abstrak dan tidak dapat diinstansiasi.");
+        System.out.println("Pesan error yang akan muncul jika dijalankan:");
+        System.out.println("\"Kendaraan is abstract; cannot be instantiated\"");
     }
-    
 }
