433bb13c92d7db131a68091a263f03df221f5b20
Septianadaw <septianadaw460@gmail.com>
2025-11-25 08:44:51 +0700

    Add files via upload

---
+++ Latihan 2 Pertemuan 12/MainLatihan2.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Latihan2Praktikum12;
+
+public class MainLatihan2 {
+    public static void main(String[] args) {
+        InventorySystem system = new InventorySystem();
+        system.run();
+    }
+}
